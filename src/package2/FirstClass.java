5efc18ab2593e330669ad044e5f02615bc22f5a0
Metin <mtnzmr@hotmail.com>
2020-08-09 09:12:39 -0600

    second commit from local repo

--- src/package2/FirstClass.java
+++ src/package2/FirstClass.java
@@ -5,6 +5,7 @@ public class FirstClass {
 	public static void main(String[] args) {
 		
 		System.out.println("this is the first change for local master");
+		System.out.println("this is second commit from local master");
 
 	}
 
