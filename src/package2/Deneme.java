9e8be547f6b614e281c991c2485482a96daf3a53
metanet007 <69377107+metanet007@users.noreply.github.com>
2020-11-22 10:53:19 -0700

    Update Deneme.java
    this is the first commit from the remote master

--- src/package2/Deneme.java
+++ src/package2/Deneme.java
@@ -7,6 +7,7 @@ public class Deneme {
 	System.out.println("this is the first changes from local master");
 	System.out.println("this is second changes from local master");
 	System.out.println("this is the third commit from loacl");
+	System.out.println("this is the first commit from remote repo");
 	
 	
 	}
