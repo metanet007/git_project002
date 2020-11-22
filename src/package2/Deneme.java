2ab8f3f9af7a88f4d8c9b5808d6fa79924222ca4
metin guclu <mtnzmr@hotmail.com>
2020-11-22 10:48:25 -0700

    second line of code

--- src/package2/Deneme.java
+++ src/package2/Deneme.java
@@ -4,7 +4,8 @@ public class Deneme {
 
 	public static void main(String[] args) {
 		
-	System.out.println("this is the first changes from local master");	
+	System.out.println("this is the first changes from local master");
+	System.out.println("this is second changes from local master");
 	
 	
 	}
